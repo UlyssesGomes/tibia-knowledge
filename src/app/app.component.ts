ccf13a16e520f7954dfd6e0255616622ec3e455a
Ulysses Gomes <ulysses_gomes@hotmail.com>
2022-10-15 17:43:45 -0300

    Lint and Husky configuration.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { PrimeNGConfig } from 'primeng/api';
 
@@ -7,7 +7,7 @@ import { PrimeNGConfig } from 'primeng/api';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'tibia-knowledge';
 
   constructor(private primengConfig: PrimeNGConfig) { }
