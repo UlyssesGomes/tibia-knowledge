ce9da1be49523512220186a4ce6ee30266963690
Ulysses Gomes <ulysses_gomes@hotmail.com>
2022-10-21 23:00:50 -0300

    Teste run husky

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('tibia-knowledge');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tibia-knowledge app is running!');
-  });
 });
